e00c501da258357fb2544f7562632990d3ed0677
Jan Munstermann <janmunstermann1995@gmail.com>
2022-06-28 13:03:59 +0200

    feat: set up initial database model via SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,28 @@
 """Initialize Flask app."""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_babel import Babel
+
+db = SQLAlchemy()
+migrate = Migrate()
+bootstrap = Bootstrap()
+babel = Babel()
 
 def init_app():
     """Construct core flask application"""
     app = Flask(__name__)
     app.config.from_object("config.Config")
-    bootstrap = Bootstrap()
-    babel = Babel()
-
+    
+    db.init_app(app)
+    migrate.init_app(app,db)
     bootstrap.init_app(app)
     babel.init_app(app)
     app.static_folder = 'static'
     with app.app_context():
         #Import parts of core flask app
-        from . import routes
+        from . import routes, models
         
         #Import dash app
         from .dashplots.pareto1 import init_pareto1
