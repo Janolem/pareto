ff6f1b12040fe759ba18e313c5e124fc4d26d7d8
Moritz Hildemann <moritz-hildemann@freenet.de>
2022-09-06 14:12:28 +0200

    formulated second and added third problem with constraints and objective functions.

--- app/db_queries.py
+++ app/db_queries.py
@@ -23,8 +23,103 @@ with application.app_context():
 import sqlalchemy_utils
 import os
 basedir = r"C:\Users\morit\PycharmProjects\pareto"
-sqlalchemy_utils.functions.drop_database('sqlite:///' + os.path.join(basedir, 'app.db'))
+#sqlalchemy_utils.functions.drop_database('sqlite:///' + os.path.join(basedir, 'app.db'))
 create_engine('sqlite:///' + os.path.join(basedir, 'app.db'))
+
+swc = Problem(name='SWC allocation optimization',
+                 description="The Soil and Water Conservation Measure Allocation problem is a "\
+                             "study conducted to find optimal allocations of installing the labor "\
+                             "intensive SWC measure bench terraces in Ethiopia. Three study areas were used: "\
+                             "Kebele Gumobila, Kebele Enerata and Kebele Mender 51."\
+                             "The study areas are affected by high soil loss rates and is known " \
+                             "to suffer from the soil losses. Gumobila located in the north-western part of Ethiopia" \
+                             "and it belongs to the West Gojjam zone in the Amhara region. " \
+                             "The altitude of Gumobila ranges from 2048 m to 3106 m above zero with a mean annual " \
+                             "rainfall of 1970 mm and is situated in the agro-ecological zone Wet Dega. " \
+                             "The second study area Enerata hasan altitude between 2283 m and 2638 m above "\
+                             "zero and mean annual rainfallof 1305 mm and is situated in the agro-ecological "\
+                             "zone called Moist Dega. The most western study area Kebele Mender 51 is classified "\
+                             "as Wet Kolla with an altitude between 1335 m and 1478 m above zero and a "\
+                             "mean annual rainfall of 1780 mm.")
+
+db.session.add(swc)
+db.session.commit()
+
+problems = Problem.query.all()
+for p in problems:
+    print(p.name)
+for p in Problem.query.filter_by(name='SWC allocation optimization'):
+    p_id = p.id
+    print(p.name)
+
+# class variation:     id, name, research_link, problem_id
+variation_Gumobila = Variation(name="Gumobila",
+                               research_link=r"https://www.researchsquare.com/article/rs-1735209/v1",
+                               problem_id=p_id)
+variation_Enerata = Variation(name="Enerata",
+                              research_link=r"https://www.researchsquare.com/article/rs-1735209/v1",
+                              problem_id=p_id)
+variation_Mender51 = Variation(name="Mender51",
+                               research_link=r"https://www.researchsquare.com/article/rs-1735209/v1",
+                               problem_id=p_id)
+
+# # id, name, description, function, mathematical_formulation
+#
+objective_1 = Objective(name="Soil Loss",
+                        description="The empirical-based Revised Universal Soil Loss Equation (RUSLE)"
+                                    "(Renard,1997) is used to estimate the soil loss of protected and unprotected "
+                                    "sub-watersheds.",
+                        mathematical_formulation=r"A = R \cdot K \cdot L \cdot S \cdot C \cdot P"
+                        ,
+                        symbols=r"A = \text{the estimated average annual soil loss and temporal average soil loss per unit of area in} t ha^{-1} yr^{-1}, "
+                                r"R = \text{Rainfall-runoff erosivity factor in} MJ mm ha^{-1} h^{-1} yr^{-1}, "
+                                r"L = Slope length factor in m, Slope steepness factor in radians,"
+                                r"S = Slope steepness factor in radians, "
+                                r"C = Cover management factor (unitless), "
+                                r"P = Support practice factor (unitless). "
+                        )
+
+objective_2 = Objective(name="Labor Requirements",
+                        description="We use empirical values of the labor requirements (Table 1) measured in" \
+                                    "person days from Tenge et al (2005) for different slopes and soil types. The soil" \
+                                    "types are categorized into stable and unstable soil, where clay soil is considered" \
+                                    "stable and loam and sand are considered unstable (Tenge et al, 2005). Soil is" \
+                                    "classified as clayey soil if the clay content is above 40%, or if the clay content" \
+                                    "is above 35% as long as the sand content is below 45% (Garc ́ıa-Gaines and" \
+                                    "Frankenstein, 2015). The labor requirement map is computed with slope, clay" \
+                                    "and sand content raster",
+                        mathematical_formulation=r"LD_{total} =A \sum_{n=1}^N ld_{n} "
+                        , symbols=r"LD_{total} = Total labor days," \
+                                  "n  = Current cell of labor requirement raster, " \
+                                  "N  = Number of cells where SWC-measures are applied, " \
+                                  "A = Cell size in ha, " \
+                                  "ld_{n} = labor days per ha."
+                        )
+db.session.commit()
+[variation_Gumobila.variation_has_objectives.append(o) for o in
+ [objective_1, objective_2]]
+db.session.commit()
+[variation_Enerata.variation_has_objectives.append(o) for o in
+ [objective_1, objective_2]]
+db.session.commit()
+[variation_Mender51.variation_has_objectives.append(o) for o in
+ [objective_1, objective_2]]
+
+db.session.commit()
+
+for p in Problem.query.filter_by(name='SWC allocation optimization'):
+    p_id = p.id
+    problem_name = p.name
+    problem_description = p.description
+
+objectives = Objective.query.join(Variation.variation_has_objectives).filter(
+    Variation.variation_has_objectives.any(id=Objective.id))
+
+for row in objectives:
+    print(row.id, row.name)
+
+
+
 comola = Problem(name='CoMOLA', description = "CoMOLA is a multi-objective constrained land use allocation problem. \
                                               It was formulated by Strauch et al. 2019 with multiple strengths of land use area and land use transition constraints.")
 
